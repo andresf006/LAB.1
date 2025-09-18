40d2a1cf4850f7dfbb032d2592228b49955f3681
andresf006 <pipemelo08@gmail.com>
2025-09-18 01:41:40 -0500

    Create promedio.py

---
+++ promedio.py
@@ -0,0 +1,2 @@
+def promedio(lista):       #Calcular promedio de la lisat
+    return sum(lista) / len(lista) if lista else 0 #si la lista no está vacía, calcula el promedio, si está vacía devuelve 0
